661cba771a0492fe84f4005f8f506f6081dda031
Eshimmel <90261234+Eshimmel@users.noreply.github.com>
2022-05-06 10:49:34 -0400

    Added Async Storage

--- views/LoginView.js
+++ views/LoginView.js
@@ -1,30 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, TextInput, Button, Alert } from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 export function LoginView({ navigation }) {
   const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [creds, setCreds] = useState({name:'', pass:''});
+  useEffect(() => {
+  const firstLoad = async () => {
+  try {
+  const savedName = await AsyncStorage.getItem("@name");
+  setUsername(savedName);
+  } catch (err) {
+  console.log(err);
+  }
+  };
+  firstLoad();
+  }, []);
+  const saveName = async () => {
+  try {
+  await AsyncStorage.setItem("@name", username);
+  alert(username)
+  } catch (err) {
+  console.log(err);
+  }
+    navigation.navigate('Home Page', {name: username, pass: password})
+  }
   return (
     <View>
       <Text>Sign Up or Sign In:</Text>
       <View>
         <TextInput
-          placeholder="Username"
+          placeholder={name}
           autoCapitalize="none"
           onChangeText={newUsername => setUsername(newUsername)}
           defaultValue={username}
         />
       </View>
-      <View>
-        <TextInput
-          placeholder="password"
-          secureTextEntry
-        />
-      </View>
       <Button
       title="Sign In"
-      onPress={() =>
-        navigation.navigate('Home Page', {name: username})
-      }/>
-      <Button title="Sign Up" />
+      onPress={saveName}/>
     </View>
   );
 }
